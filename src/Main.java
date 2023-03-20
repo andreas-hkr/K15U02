a2a3a18b315dc85303c5e8f1d7d2c87fc971da7e
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 13:59:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+        // Gör objekt av klassen Card naturligt jämförbara.
+        //  - compareTo
+        //      -1 om "vårt kort" är minst
+        //       0 on lika
+        //       1 om "det andra kortet" är minst
+
+
+    }
+}
+
+class Card {
+    static final int CLUBS = 0;
+    static final int DIAMONDS = 1;
+    static final int HEARTS = 2;
+    static final int SPADES = 3;
+    static String[] suits = {"Klöver", "Ruter", "Hjärter", "Spader"};
+    static String[] ranks = {
+            "ess", "två", "tre", "fyra", "fem", "sex", "sju",
+            "åtta", "nio", "tio", "knekt", "dam", "kung"
+    };
+
+    int suit;
+    int rank;
+
+    @Override
+    public String toString() {
+        return suits[suit] + " " + ranks[rank-1];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Card c) {
+            return suit == c.suit && rank == c.rank;
+        }
+        return false;
+    }
+
+    void change(int suit, int rank) {
+        if ((suit >= 0 && suit <= 3) && (rank >= 0 && rank <= 13)) {
+            this.suit = suit;
+            this.rank = rank;
+        } else {
+            throw new IllegalArgumentException("Felaktigt kort");
+        }
+    }
+}
